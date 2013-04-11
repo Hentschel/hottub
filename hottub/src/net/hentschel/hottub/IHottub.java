1267cdf1bd1123823afb74a9267a23836cc3fa8e
HentschelT <thomas.hentschel@novellus.com>
2013-04-11 00:05:23 -0700

    some license pruning

--- hottub/src/net/hentschel/hottub/IHottub.java
+++ hottub/src/net/hentschel/hottub/IHottub.java
@@ -2,14 +2,6 @@
  * Class: IHeater
  * 
  * Created on Apr 8, 2013
- * 
- * (c) Copyright Novellus Systems, Inc., unpublished work, created 2003
- * All use, disclosure, and/or reproduction of this material is prohibited
- * unless authorized in writing.  All Rights Reserved.
- * Rights in this program belong to:
- * Novellus Systems, Inc.
- * 4000 N. First Street
- * San Jose, CA
  */
 package net.hentschel.hottub;
 
